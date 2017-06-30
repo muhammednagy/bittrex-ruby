4b707b310d4cae0accbe2e7bcbceafcba349cbcb
Sergey <dead-black_93@mail.ru>
2017-06-30 15:55:14 +0300

    refactoring frame message class

--- lib/bittrex/websocket/frame/message.rb
+++ lib/bittrex/websocket/frame/message.rb
@@ -1,16 +1,19 @@
 class Bittrex::WebSocket::Frame
   class Message
-    MESSAGE_TYPES = [
-      'updateSummaryState',
-      'updateExchangeState'
-    ].freeze
+
+    HUB_KEY  = 'H'.freeze
+    TYPE_KEY = 'M'.freeze
+    DATA_KEY = 'A'.freeze
+
+    UPDATE_MARKETS_SUMMARY_TYPE = 'updateSummaryState'.freeze
+    UPDATE_EXCHANGE_STATE_TYPE  = 'updateExchangeState'.freeze
 
     attr_reader :hub, :type, :data
 
     def initialize(message_data)
-      @hub      = message_data['H']
-      @type     = message_data['M']
-      @data     = message_data['A']
+      @hub      = message_data[HUB_KEY]
+      @type     = message_data[TYPE_KEY]
+      @data     = message_data[DATA_KEY]
     end
   end
 end
